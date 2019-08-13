a94a33a54fb435b86f6ad4ef8b7f0e14821fa140
zonghai.zhu <zonghai.zhu@nio.com>
2019-08-13 13:52:04 +0800

    增加自动验证(Validation可用）

--- GinDemos/gin-blog/routers/api/v1/article.go
+++ GinDemos/gin-blog/routers/api/v1/article.go
@@ -201,13 +201,13 @@ func GetArticles(c *gin.Context) {
 // }
 
 type AddArticleForm struct {
-	TagID         int    `form:"tag_id" json:"tag_id"`
-	Title         string `form:"title" json:"title"`
-	Desc          string `form:"desc" json:"desc"`
-	Content       string `form:"content" json:"content"`
-	CreatedBy     string `form:"created_by" json:"created_by"`
-	CoverImageUrl string `form:"cover_image_url" json:"cover_image_url"`
-	State         int    `form:"state" json:"state"`
+	TagID         int    `form:"tag_id" json:"tag_id" valid:"Required;Min(1)"`
+	Title         string `form:"title" json:"title" valid:"Required;MaxSize(100)"`
+	Desc          string `form:"desc" json:"desc" valid:"Required;MaxSize(255)"`
+	Content       string `form:"content" json:"content" valid:"Required;MaxSize(65535)"`
+	CreatedBy     string `form:"created_by" json:"created_by" valid:"Required;MaxSize(100)"`
+	CoverImageUrl string `form:"cover_image_url" json:"cover_image_url" valid:"Required;MaxSize(255)"`
+	State         int    `form:"state" json:"state" valid:"Range(0,1)"`
 }
 
 // @Summary 新增文章
