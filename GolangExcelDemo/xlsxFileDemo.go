78c6bcb8b761f7f252f1bd9107a48031ad45fca7
yudiefly <zzh203@yeah.net.cn>
2020-10-11 21:37:46 +0800

    add GolangExcelDemo/xlsxFileDemo.go

---
+++ GolangExcelDemo/xlsxFileDemo.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/tealeg/xlsx"
+)
+
+func main() {
+	file := xlsx.NewFile()
+	sheet, err := file.AddSheet("人员信息收集")
+	if err != nil {
+		panic(err.Error())
+	}
+	row := sheet.AddRow()
+	cell := row.AddCell()
+	cell.Value = "姓名"
+	cell = row.AddCell()
+	cell.Value = "性别"
+
+	row = sheet.AddRow()
+	cell = row.AddCell()
+	cell.Value = "张三"
+	cell = row.AddCell()
+	cell.Value = "男"
+
+	row = sheet.AddRow()
+	cell = row.AddCell()
+	cell.Value = "李四"
+	cell = row.AddCell()
+	cell.Value = "女"
+
+	err = file.Save("demo.xlsx")
+	if err != nil {
+		panic(err.Error())
+	}
+
+}
